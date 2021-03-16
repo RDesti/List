4c508c198f0df171e89a5d546921b7e3a52b49ab
RDesti <tatotschka17@rambler.ru>
2021-03-16 16:21:05 +0200

    initial

---
+++ List/ArrayList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace List
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            Length = 0;
+
+            _array = new int[10];
+        }
+
+        public void Add(int value)
+        {
+            if(Length == _array.Length)
+            {
+                UpSize();
+            }
+            _array[Length] = value;
+            Length++;
+        }
+        private void UpSize()
+        {
+             int newLength = (int)(_array.Length * 1.33d + 1);
+
+            int[] tmpArray = new int[newLength];
+            for(int i = 0; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+            _array = tmpArray;
+        }
+    }
+}
