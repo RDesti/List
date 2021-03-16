8a4c8cd2193fb267ab6ee77f8d8528cde3484d2d
RDesti <tatotschka17@rambler.ru>
2021-03-16 20:25:47 +0200

    added AddToBeginning + AddByIndex

--- List/ArrayList.cs
+++ List/ArrayList.cs
@@ -24,6 +24,42 @@ namespace List
             _array[Length] = value;
             Length++;
         }
+        public void AddToBeginning(int value)
+        {
+            if(Length == _array.Length)
+            {
+                UpSize();
+            }
+            int[] tmpArray = new int[_array.Length + 1];
+            for(int i = 1; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i - 1];
+            }
+            tmpArray[0] = value;
+            _array = tmpArray;
+            Length++;
+        }
+        public void AddByIndex(int index, int value)
+        {
+            if(Length == _array.Length)
+            {
+                UpSize();
+            }
+            int i;
+            int[] tmpArray = new int[_array.Length + 1];
+            for(i = 0; i < index; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+            int a = index + 1;
+            for(i = a; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i - 1];
+            }
+            tmpArray[index] = value;
+            _array = tmpArray;
+        }
+
         private void UpSize()
         {
              int newLength = (int)(_array.Length * 1.33d + 1);
