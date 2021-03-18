f5ee64456b12c7fb245ee229e20cd9ef57bbbcf1
RDesti <tatotschka17@rambler.ru>
2021-03-18 13:46:09 +0200

    added GetFirstIndex+Change+GetReverst+Find(Max, min, indexMax, indexMin

--- List/ArrayList.cs
+++ List/ArrayList.cs
@@ -73,6 +73,10 @@ namespace List
         }
         public void RemoveByIndex(int index, int countElements)
         {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException("Sorry, but you are accessing a non-existent index");
+            }
             Length -= countElements;
             MoveLeftToIndex(index, countElements);
             Resize();
@@ -83,41 +87,116 @@ namespace List
         }
         public void GetIndexAccess(int index)
         {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException("Sorry, but you are accessing a non-existent index");
+            }
             int valueByIndex = _array[index];
         }
-
-
-
-
-
+        public void GetFirstIndex(int value)
+        {
+            int firstIndex = -1;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    firstIndex = i;
+                }
+            }
+        }
+        public void Change(int index, int value)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException("Sorry, but you are accessing a non-existent index");
+            }
+            _array[index] = value;
+        }
+        public void GetReverst(ref int[] _array)
+        {
+            for (int i = 0; i < Length / 2; i++)
+            {
+                int revers = _array[i];
+                _array[i] = _array[Length - i - 1];
+                _array[Length - i - 1] = revers;
+            }
+        }
+        public void FindMax(int[] _array)
+        {
+            int max = _array[0];
+            for (int i = 1; i < Length; i++)
+            {
+                if (max < _array[i])
+                {
+                    max = _array[i];
+                }
+            }
+        }
+        public void FindMin(int[] _array)
+        {
+            int min = _array[0];
+            for (int i = 1; i < Length; i++)
+            {
+                    if (min > _array[i])
+                    {
+                        min = _array[i];
+                    }
+            }
+        }
+        public void FindIndexMaxElement(int[] _array)
+        {
+            int max = _array[0];
+            int maxIndex = 0;
+            for (int i = 1; i < Length; i++)
+            {
+                if (max < _array[i])
+                {
+                    max = _array[i];
+                    maxIndex = i;
+                }
+            }
+        }
+        public void FindIndexMinElement(int[] _array)
+        {
+            int min = _array[0];
+            int minIndex = 0;
+            for (int i = 1; i < Length; i++)
+            {
+                if (min > _array[i])
+                {
+                    min = _array[i];
+                    minIndex = i;
+                }
+            }
+        }
 
 
 
 
 
         private void Resize()
-        {
-            int newLength = (int)(Length * 1.33 + 1);
-            int[] tmpArray = new int[newLength];
-            for (int i = 0; i < _array.Length; i++)
             {
-                tmpArray[i] = _array[i];
+                int newLength = (int)(Length * 1.33 + 1);
+                int[] tmpArray = new int[newLength];
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    tmpArray[i] = _array[i];
+                }
+                _array = tmpArray;
             }
-            _array = tmpArray;
-        }
-        private void MoveRightToIndex(int index, int count)
-        {
-            for (int i = Length-1; i >= index; i--)
+            private void MoveRightToIndex(int index, int count)
             {
-                _array[i + count] = _array[i];
+                for (int i = Length - 1; i >= index; i--)
+                {
+                    _array[i + count] = _array[i];
+                }
             }
-        }
-        private void MoveLeftToIndex(int index, int count)
-        {
-            for (int i = index; i < Length; i++)
+            private void MoveLeftToIndex(int index, int count)
             {
-                _array[i] = _array[i + count];
+                for (int i = index; i < Length; i++)
+                {
+                    _array[i] = _array[i + count];
+                }
             }
-        }
-    }
+    } 
 }
